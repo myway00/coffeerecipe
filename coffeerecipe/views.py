bd6c5d7252f1b528705894093af79ccc2c45fada
su-pernova <sumikim3m323@naver.com>
2021-05-01 16:01:08 +0900

    sumi

--- coffeerecipe/views.py
+++ coffeerecipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Content
 from .forms import ContentForm
@@ -24,3 +24,26 @@ def new(request):
     
     return render(request, 'new.html', {'form':form})
 
+def detail(request, index):
+    # get_object_or_404 : 객체를 가져오되, 객체가 없다면 404 에러를 띄워주는 함수
+    post = get_object_or_404(Content, pk=index)
+    return render(request, 'detail.html', {'post':post})
+
+def edit(request, index):
+    post = get_object_or_404(Content, pk=index)
+    if request.method == "POST":
+        form = ContentForm(request.POST, instance=post) # post가 가지고 있는 정보를 넘겨줄 것임
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now
+            post.save()
+            return redirect('detail', index = post.pk)
+    else:
+        form = ContentForm(instance=post)
+    return render(request, 'edit.html', {'form':form})
+
+def delete(request, pk): # index 대신 pk를 사용한다
+    post = get_object_or_404(Content, pk=pk)
+    post.delete() # 이것만 하면 끝남!
+    return redirect('home')
