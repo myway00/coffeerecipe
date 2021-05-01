5db5f6bb25e36834435387d276ff13bdf57e96db
su-pernova <sumikim3m323@naver.com>
2021-05-01 17:23:33 +0900

    sumi

--- coffeerecipe/forms.py
+++ coffeerecipe/forms.py
@@ -4,4 +4,4 @@ from .models import Content
 class ContentForm(forms.ModelForm):
     class Meta:
         model = Content
-        fields = ['title', 'body', ]
+        fields = ['title', 'body', 'taste', 'size', ]
--- coffeerecipe/models.py
+++ coffeerecipe/models.py
@@ -7,3 +7,20 @@ class Content(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField(default='')
+
+    TASTE_CHOICES = [
+        ('인생의 쓴맛', '인생의 쓴맛'),
+        ('어우달아!', '어우달아!'),
+        ('밍밍한 한강물맛', '밍밍한 한강물맛'),
+        ('마트..다녀오셨어요?', '마트..다녀오셨어요?'),
+        ('매운맛', '매운맛'),
+    ]
+    taste = models.CharField(max_length=30, choices=TASTE_CHOICES, default='인생의 쓴맛')
+    
+    SIZE_CHOICES = [
+        ('아리아나그란데', '아리아나그란데'),
+        ('그란데말입니다', '그란데말입니다'),
+        ('그란데밖에없는데', '그란데밖에없는데'),
+        ('손에 담아마실게요', '손에 담아마실게요')
+    ]
+    size = models.CharField(max_length=30, choices=SIZE_CHOICES, default='아리아나그란데')
